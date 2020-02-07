6fdd4b3c35d7648bb0cbe38ed579dbb9f5ae56c6
Brady John Geleynse <geleynb@cf405-16.cs.wwu.edu>
2020-02-07 14:02:10 -0800

    push

--- webview/js/codequest_blocks.js
+++ webview/js/codequest_blocks.js
@@ -21,25 +21,25 @@ function generateBlock(id) {
     else
       block.setAttribute('type', id);
 
-    var values = block_entry['values'].split(';');
-    for (var i = 0; i < values.length; i++) {
-      var value_cascade = value[i].split('>');
-      var value = document.createElement('value');
-      value.setAttribute('name', value_cascade[0]);
-      block.appendChild(value);
-      if (value_cascade[1] != '') {
-        var shadow = document.createElement('shadow');
-        shadow.setAttribute('type', value_cascade[1]);
-        value.appendChild(shadow);
-        if (value_cascade[2] != '') {
-          var field = document.createElement('field');
-          field.setAttribute('name', value_cascade[2]);
-          if (value_cascade[3] != '')
-            field.innerHTML = value_cascade[3];
-          shadow.appendChild(field);
-        }
-      }
-    }
+    // var values = block_entry['values'].split(';');
+    // for (var i = 0; i < values.length; i++) {
+    //   var value_cascade = value[i].split('>');
+    //   var value = document.createElement('value');
+    //   value.setAttribute('name', value_cascade[0]);
+    //   block.appendChild(value);
+    //   if (value_cascade[1] != '') {
+    //     var shadow = document.createElement('shadow');
+    //     shadow.setAttribute('type', value_cascade[1]);
+    //     value.appendChild(shadow);
+    //     if (value_cascade[2] != '') {
+    //       var field = document.createElement('field');
+    //       field.setAttribute('name', value_cascade[2]);
+    //       if (value_cascade[3] != '')
+    //         field.innerHTML = value_cascade[3];
+    //       shadow.appendChild(field);
+    //     }
+    //   }
+    // }
 
     document.getElementById(block_entry['id'].split('_')[0]).appendChild(block);
   }
