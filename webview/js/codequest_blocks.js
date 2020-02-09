759eedbedc2084c1bedc442dae20922a2144a344
Brady Geleynse <bradyjg@outlook.com>
2020-02-09 14:55:26 -0800

    Block data is stored in Google Sheets and is imported and saved in localStorage, will update on version change. Also created a Google Sheet to import the blocks used for each problem.

--- webview/js/codequest_blocks.js
+++ webview/js/codequest_blocks.js
@@ -1,15 +1,14 @@
 function generateBlock(id) {
-  console.log('generateBlock():', id);
+  console.log('generateBlock()', id)
   var block_entry = null;
-  for (entry in BLOCK_LIST)
-    if (BLOCK_LIST[entry]['id'] == id)
-      block_entry = BLOCK_LIST[entry];
-  if (block_entry != null) {
+  for (entry in blockList)
+    if (blockList[entry]['id'] == id)
+      block_entry = blockList[entry];
 
+  if (block_entry != null) {
     var mutations = block_entry['mutations'].split(';');
     var block = document.createElement('block');
     if (mutations[0] != '') {
-      console.log(mutations[0])
       block.setAttribute('type', id.split('-')[0])
       for (var i = 0; i < mutations.length; i++) {
         var attribute = mutations[i].split('=');
@@ -21,26 +20,25 @@ function generateBlock(id) {
     else
       block.setAttribute('type', id);
 
-    // var values = block_entry['values'].split(';');
-    // for (var i = 0; i < values.length; i++) {
-    //   var value_cascade = value[i].split('>');
-    //   var value = document.createElement('value');
-    //   value.setAttribute('name', value_cascade[0]);
-    //   block.appendChild(value);
-    //   if (value_cascade[1] != '') {
-    //     var shadow = document.createElement('shadow');
-    //     shadow.setAttribute('type', value_cascade[1]);
-    //     value.appendChild(shadow);
-    //     if (value_cascade[2] != '') {
-    //       var field = document.createElement('field');
-    //       field.setAttribute('name', value_cascade[2]);
-    //       if (value_cascade[3] != '')
-    //         field.innerHTML = value_cascade[3];
-    //       shadow.appendChild(field);
-    //     }
-    //   }
-    // }
-
+    var values = block_entry['values'].split(';');
+    for (var i = 0; i < values.length; i++) {
+      var value_cascade = values[i].split('>');
+      var value = document.createElement('value');
+      value.setAttribute('name', value_cascade[0]);
+      block.appendChild(value);
+      if (value_cascade[1] != undefined) {
+        var shadow = document.createElement('shadow');
+        shadow.setAttribute('type', value_cascade[1]);
+        value.appendChild(shadow);
+        if (value_cascade[2] != undefined) {
+          var field = document.createElement('field');
+          field.setAttribute('name', value_cascade[2]);
+          if (value_cascade[3] != undefined)
+            field.innerHTML = value_cascade[3];
+          shadow.appendChild(field);
+        }
+      }
+    }
     document.getElementById(block_entry['id'].split('_')[0]).appendChild(block);
   }
 }
