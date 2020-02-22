c3b3a6b547b6193b42061d26d5b3cc9cde67d802
Brady John Geleynse <geleynb@linux-02.cs.wwu.edu>
2020-02-22 00:27:32 -0800

    Not working. Just need to transfer to home machine.

--- webview/js/codequest_blocks.js
+++ webview/js/codequest_blocks.js
@@ -71,6 +71,7 @@ function createToolboxCategory(object) {
 // Creates the XML Toolbar object by given ID denoted in Google Sheets
 function generateBlock(id) {
   console.log('generateBlock()', id)
+  // Finds block in 
   if (id.split('_')[0] != 'createvar' && id.split('_')[0] != 'createfun') {
     var block_entry = null;
     for (entry in blockList) {
@@ -82,42 +83,7 @@ function generateBlock(id) {
     }
 
     if (block_entry != null) {
-      var mutations = block_entry['mutations'].split(';');
-      var block = document.createElement('block');
-      if (mutations[0] != '') {
-        block.setAttribute('type', id.split('-')[0])
-        for (var i in mutations) {
-          var attribute = mutations[i].split('=');
-          var mutation = document.createElement('mutation');
-          mutation.setAttribute(attribute[0], attribute[1].replace(/\"/g, ''));
-          block.appendChild(mutation);
-        }
-      }
-      else
-        block.setAttribute('type', id);
-
-      var values = block_entry['values'].split(';');
-      for (var i in values) {
-        var value_cascade = values[i].split('>');
-        if (value_cascade[0] != undefined) {
-          var value = document.createElement('value');
-          value.setAttribute('name', value_cascade[0]);
-          block.appendChild(value);
-          if (value_cascade[1] != undefined) {
-            var shadow = document.createElement('shadow');
-            shadow.setAttribute('type', value_cascade[1]);
-            value.appendChild(shadow);
-            if (value_cascade[2] != undefined) {
-              var field = document.createElement('field');
-              field.setAttribute('name', value_cascade[2]);
-              if (value_cascade[3] != undefined)
-                field.innerHTML = value_cascade[3];
-              shadow.appendChild(field);
-            }
-          }
-        }
-      }
-
+      
       if (block_entry['id'].trim().indexOf('_') > 0) {
         createToolboxCategory(categories.find(item => {
           return item.id == block_entry['id'].trim().split('_')[0]
