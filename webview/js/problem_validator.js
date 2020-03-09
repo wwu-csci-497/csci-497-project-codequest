2810d4894be3e883387d73103635909a47df17cc
Brady Geleynse <brady@novatechwa.com>
2020-03-09 15:13:15 -0700

    build you can press play

--- webview/js/problem_validator.js
+++ webview/js/problem_validator.js
@@ -2,10 +2,13 @@ function testAnswer(curProb, code) {
   // return [true, {}];
   switch(parseInt(curProb)) {
     case 0000:
-      blockList[curProb]['solutions'].forEach(element => {
-        if (strCompare(element, code))
+      for (var i = 0; i < blockList[curProb]['solutions'].length; i++) {
+        if (strCompare(blockList[curProb]['solutions'][i], code)) {
+          console.log('element<->code is true');
           return [true, {}];
-      });
+        }
+      }
+      console.log('element<->code is false');
       return [false, {}];
     case 0001:
       blockList[curProb]['solutions'].forEach(element => {
@@ -53,13 +56,20 @@ function testAnswer(curProb, code) {
 }
 
 function strCompare(s1, s2) {
-  if (s1.length != s2.length)
+  if (s1 == null || s1 == null) {
+    console.error('strCompare()', 'One of the two strings are null');
+    return false
+  }
+  if (s1.length != s2.length) {
+    console.error('strCompare()', 'Strings are not the same length.');
     return false;
+  }
   for (var i = 0; i < s1.length; i++) {
     if (s1[i] != s2[i]) {
-      console.log('s1[' + i + ']: ' + s1[i] + '\ns2[' + i + ']: ' + s2[i])
+      console.error('strCompare()', 's1[' + i + ']: ' + s1[i] + '\ns2[' + i + ']: ' + s2[i])
       return false;
     }
   }
+  console.error('strCompare()', 'true')
   return true;
 }
