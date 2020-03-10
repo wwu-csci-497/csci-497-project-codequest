bec8ce13b867b10398d03fdf7daa198cf255ec82
Raleigh Hansen <hansen92@wwu.edu>
2020-03-10 00:11:43 -0700

    gotta commit before I can pull

--- webview/js/problem_validator.js
+++ webview/js/problem_validator.js
@@ -38,8 +38,19 @@ function testAnswer(curProb, code) {
       return [false, {}];
     case 1004:
       for (var i = 0; i < blockList[curProb]['solutions'].length; i++)
-        if (strCompare(blockList[curProb]['solutions'][i], code))
-          return [true, {}];
+        var s1 = blockList[curProb]['solutions'][i];
+        var s2 = code;
+        if (s1 == null || s2 == null) {
+          console.error('strCompare()', 'One of the two strings are null');
+          return [false, []];
+        }
+        if (s1.length != s2.length) {
+          console.error('strCompare()', 'Strings are not the same length -- s1: ' + s1.length + ' s2: ' + s2.length);
+          return [false, []];
+        }
+
+        if (gemsParse(code))
+          return [true, ["Yellow", "Purple", "Blue", "Orange", "Green"]];
       return [false, {}];
     default:
       return [false, {}];
@@ -49,7 +60,7 @@ function testAnswer(curProb, code) {
 function strCompare(s1, s2) {
   if (s1 == null || s2 == null) {
     console.error('strCompare()', 'One of the two strings are null');
-    return false
+    return false;
   }
   if (s1.length != s2.length) {
     console.error('strCompare()', 'Strings are not the same length -- s1: ' + s1.length + ' s2: ' + s2.length);
@@ -107,4 +118,45 @@ function beetleParse(newcode) {
     c++;
   }
   return data;
+}
+
+function gemsParse(code) {
+  var i = 0;
+  var c = 0;
+  while (i >= 0) {
+    if (c == 5)
+      break;
+    var color = '';
+    i+=4;
+    while (newcode[i] != ')')
+      color += newcode[i++];
+    i+=6;
+    var dir = '';
+    while (newcode[i] != ' ')
+      dir += newcode[i++];
+    i+=3;
+    var num = '';
+    while (newcode[i] != ';')
+      num += newcode[i++];
+    num = parseInt(num);
+
+    if (dir == 'move_left_count')
+      num*=-1;
+
+    console.log(color, dir, num);
+
+    if (strCompare(color, 'green'))
+      data[0] = num;
+    else if (strCompare(color, 'blue'))
+      data[1] = num;
+    else if (strCompare(color, 'purple'))
+      data[2] = num;
+    else
+      console.error('beetleParse()', 'Not a valid color: ' + color);
+
+    console.log(i);
+    i = newcode.indexOf('if (', i);
+    console.log(i);
+    c++;
+  }
 }
